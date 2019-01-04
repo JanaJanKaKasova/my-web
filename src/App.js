ca13e40516478e79d30a56d757a3cdbb2bd13beb
Jana Kasova <ja.kasova@seznam.cz>
2019-01-04 18:06:02 +0100

    Created Navbar.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <div>Hello World!</div>
         <h1>Hellooo</h1>
       </div>
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Navbar extends Component {
+  render() {
+    return <div>tady je Navbar</div>;
+  }
+}
+
+export default Navbar;
