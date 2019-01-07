24fb936b262f7b3bcc7e21bcff254d3432326458
Jana Kasova <ja.kasova@seznam.cz>
2019-01-07 23:22:30 +0100

    Added main_pic

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import main_pic from "./pics/main_pic.jpg";
 
 class Header extends Component {
   render() {
     return (
       <div className="conatiner-fluid">
-        <div className="row pt-5">
-          <div className="col-sm">bz</div>
+        <div className="row pt-4">
+          <div className="col-sm main_pic">
+            <img src={main_pic} alt="Main_pic" />
+          </div>
           <div className="col-sm">fň</div>
         </div>
       </div>
