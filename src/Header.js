e0fc96e9adadbbeef86f76567214b9199f3dfc3b
Jana Kasova <ja.kasova@seznam.cz>
2019-01-08 21:07:49 +0100

    changed Header

--- src/Header.js
+++ src/Header.js
@@ -4,14 +4,17 @@ import main_pic from "./pics/main_pic.jpg";
 class Header extends Component {
   render() {
     return (
-      <div className="conatiner-fluid">
+      <div className="conatiner-fluid header_container">
         <div className="row pt-4">
-          <div className="col-sm main_pic">
-            <img src={main_pic} alt="Main_pic" />
+          <div className="main_pic">
+            {/* <img src={main_pic} alt="Main_pic" /> */}
+            <div className="quote">
+              <h1>"Dreams don't work unless we do"</h1>
+            </div>
           </div>
-          <div className="col-sm quote">
+          {/* <div className="col-sm quote">
             <h1>"Dreams don't work unless we do"</h1>
-          </div>
+    </div> */}
         </div>
       </div>
     );
