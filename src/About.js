25913cf97d113904003f4ead872faaf869aa9c29
Jana Kasova <ja.kasova@seznam.cz>
2019-01-14 11:40:59 +0100

    changed style in About

--- src/About.js
+++ src/About.js
@@ -5,29 +5,31 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <div className="row pt-3">
+        <div className="row pt-5">
           <div className="col-sm" />
           <div className="col-sm heading">
             <h1>...so I do</h1>
           </div>
         </div>
-        <div className="row">
+        <div className="row pt-5">
           <div className="col-sm-2" />
-          <div className="col-sm-10">
+          <div className="col-sm-8 about_copy">
             <h2>My name is Jana</h2>
           </div>
+          <div className="col-sm-2" />
         </div>
         <div className="row">
           <div className="col-sm-2" />
-          <div className="col-sm-3">
+          <div className="col-sm-8 about_copy">
             <h3>
               I am Content Creator, Junior Front-End Developer & Surf Instructor
             </h3>
           </div>
+          <div className="col-sm-2" />
         </div>
         <div className="row">
           <div className="col-sm-2" />
-          <div className="col-sm-4">
+          <div className="col-sm-8 about_copy">
             <p>
               Bio .......... Lorem ipsum dolor sit amet, consectetuer adipiscing
               elit. Aliquam erat volutpat. In sem justo, commodo ut, suscipit
@@ -36,6 +38,7 @@ class About extends Component {
               suscipit, sollicitudin et, dolor. In rutrum.
             </p>
           </div>
+          <div className="col-sm-2" />
         </div>
       </div>
     );
